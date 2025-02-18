381c291d694292a01f6c1a2b682336a29871cf8d
Kaempy <campa.banj@gmail.com>
2025-02-18 08:38:02 +0100

    chore: update on upload component

--- src/components/ui/FormUpload.tsx
+++ src/components/ui/FormUpload.tsx
@@ -1,6 +1,6 @@
 import Download from '@src/icons/download';
 import { Paperclip } from 'lucide-react';
-import { useCallback } from 'react';
+import { Fragment, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Controller, useFormContext } from 'react-hook-form';
 import { Button } from './button';
@@ -46,36 +46,39 @@ const FormUpload = ({ name, label }: UploadProps) => {
             <FormLabel className="text-sm font-medium text-title">
               {label}
             </FormLabel>
-            {filename ? (
-              <p className="flex w-full items-center gap-2 rounded-md border border-dashed border-[#ABA7AF] p-4 text-sm text-black">
-                <Paperclip size={12} />
-                {filename}
-              </p>
-            ) : (
-              <div
-                className="flex h-full max-h-[200px] w-full cursor-pointer flex-col items-center justify-start gap-2 rounded border border-dashed border-[#ABA7AF] bg-transparent px-[20px] py-[12px] text-black hover:border-black hover:bg-transparent hover:text-black disabled:bg-[#F4F7FA]"
-                {...getRootProps()}
-              >
-                <Download />
-                <small className="text-sm text-title">
-                  Drag here or click the button below to upload
-                </small>
-                <Button type="button">
-                  <Paperclip />
-                  <span>Choose File</span>
-                </Button>
-                <span className="text-[#4B3A5A]">
-                  Maximum upload size: 2MB (.jpeg)
-                </span>
-                <input
-                  id={name}
-                  type="file"
-                  hidden
-                  accept="image/jpeg"
-                  {...getInputProps({ onChange })}
-                />
-              </div>
-            )}
+
+            <div
+              className="flex h-full max-h-[200px] w-full cursor-pointer flex-col items-center justify-start gap-2 rounded border border-dashed border-[#ABA7AF] bg-transparent px-[20px] py-[12px] text-black hover:border-black hover:bg-transparent hover:text-black disabled:bg-[#F4F7FA]"
+              {...getRootProps()}
+            >
+              {filename ? (
+                <p className="flex w-full items-center gap-2 text-sm text-black">
+                  <Paperclip size={12} />
+                  {filename}
+                </p>
+              ) : (
+                <Fragment>
+                  <Download />
+                  <small className="text-sm text-title">
+                    Drag here or click the button below to upload
+                  </small>
+                  <Button type="button">
+                    <Paperclip />
+                    <span>Choose File</span>
+                  </Button>
+                  <span className="text-[#4B3A5A]">
+                    Maximum upload size: 2MB (.jpeg)
+                  </span>
+                </Fragment>
+              )}
+              <input
+                id={name}
+                type="file"
+                hidden
+                accept="image/jpeg"
+                {...getInputProps({ onChange })}
+              />
+            </div>
             {error && <p className="text-sm text-red-500">{error.message}</p>}
           </FormItem>
         );
